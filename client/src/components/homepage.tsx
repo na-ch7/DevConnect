be0c6fcebcd98c151dc10196ab886778216b6f62
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-07-27 18:04:17 +0530

    feat: homepage initialized

---
+++ client/src/components/homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage(){
+    return (
+        <div className="text-spotify">
+            <div className="text-4xl text-black"><p>DEVCONNECT</p></div>
+        </div>
+    );
+}
+
+export default HomePage;
