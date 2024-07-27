eee44848516b180668fb7c108f46087e1d759b23
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2024-07-27 18:12:30 +0530

    feat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router , Route , Routes} from 'react-router-dom'
+import HomePage from './components/homepage';
+import Navbar from './components/navbar'
+import './assets/fonts/font.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app-container bg-periwinkle'>
+        <Navbar/>
+        <div>
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+        </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
