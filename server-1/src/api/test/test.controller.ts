7fb71e2b6babebb71d746877ec47bce74bce8584
na-ch7 <141823845+na-ch7@users.noreply.github.com>
2024-07-28 08:54:15 +0530

    feat: profile

--- server-1/src/api/test/test.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-export default (): Router => {
-  const app = Router();
-  app.get('/', (_req, res) => {
-    res.send('Hello World!');
-  });
-  return app;
-};
